b172f88f67bf423d43b314a3245c3980d31f7323
harrybandukda <harrisbandukda@gmail.com>
2025-02-13 04:47:25 -0800

    Initial Commit

---
+++ LabTestOne/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  LabTestOne
+//
+//  Created by harry on 25/11/1403 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
---
+++ LabTestOne/LabTestOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  LabTestOneApp.swift
+//  LabTestOne
+//
+//  Created by harry on 25/11/1403 AP.
+//
+
+import SwiftUI
+
+@main
+struct LabTestOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
